580625acf330354abc634e872799958b95e41547
A-STAR <andreysuperstar@gmail.com>
2025-04-19 01:01:53 +0300

    feat(client): set up wallet client with account
    Set up JSON-RPC account.
    
    Set up wallet client account.
    
    Consume action (send transaction).

--- 04-wallet-client/index.ts
+++ 04-wallet-client/index.ts
@@ -1,8 +1,11 @@
 import { createWalletClient, custom } from 'viem'
 import { mainnet, sepolia } from 'viem/chains'
 
+export const [account] = await window.ethereum!.request({ method: 'eth_requestAccounts' })
+
 // eg: Metamask
 export const client = createWalletClient({
+  // account,
   // chain: mainnet,
   chain: sepolia,
   transport: custom(window.ethereum!)
