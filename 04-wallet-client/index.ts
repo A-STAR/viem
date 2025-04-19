c8c09f6ec6d748183681eb6932d7b067ee319a15
A-STAR <andreysuperstar@gmail.com>
2025-04-19 09:12:24 +0300

    feat(client): set up wallet with private key

--- 04-wallet-client/index.ts
+++ 04-wallet-client/index.ts
@@ -1,12 +1,17 @@
 import { createWalletClient, custom } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
 import { mainnet, sepolia } from 'viem/chains'
 
 export const [account] = await window.ethereum!.request({ method: 'eth_requestAccounts' })
+// const account = privateKeyToAccount('0x...')
 
 // eg: Metamask
 export const client = createWalletClient({
   // account,
+  // account: '0x...',
   // chain: mainnet,
   chain: sepolia,
   transport: custom(window.ethereum!)
 })
+
+// export const account = privateKeyToAccount('0x...')
