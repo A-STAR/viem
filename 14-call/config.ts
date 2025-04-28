40aea67e10c9811663c4a5df47f000ab934d6a09
A-STAR <andreysuperstar@gmail.com>
2025-04-28 16:28:32 +0300

    feat(call): make deployless call

---
+++ 14-call/config.ts
@@ -0,0 +1,22 @@
+import { createPublicClient, http } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { mainnet } from 'viem/chains'
+
+// JSON-RPC Account
+export const account = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+
+// Local Account
+// export const account = privateKeyToAccount(...)
+
+export const publicClient = createPublicClient({
+  // chain: mainnet,
+  chain: {
+    ...mainnet,
+    rpcUrls: {
+      default: {
+        http: ['https://eth.drpc.org'],
+      },
+    },
+  },
+  transport: http(),
+})
