957b316d6a04a3141b196787112760f3cfb1fe1e
A-STAR <andreysuperstar@gmail.com>
2025-05-02 02:25:44 +0300

    feat(EIP-712): get undeployed EIP-712 domain

--- 17-eip712/client.ts
+++ 17-eip712/client.ts
@@ -1,5 +1,10 @@
-import { http, createPublicClient } from 'viem'
+import { createPublicClient, http, parseAbi } from 'viem'
 import { mainnet } from 'viem/chains'
+
+export const factory = {
+  address: '0xE8Df82fA4E10e6A12a9Dab552bceA2acd26De9bb',
+  abi: parseAbi(['function createAccount(address owner, uint256 salt)']),
+} as const
 
 export const publicClient = createPublicClient({
   // chain: mainnet,
--- 17-eip712/index.ts
+++ 17-eip712/index.ts
@@ -1,7 +1,21 @@
-import { publicClient } from './client'
+import { encodeFunctionData } from 'viem'
+import { factory, publicClient } from './client'
 
-const { domain, extensions, fields } = await publicClient.getEip712Domain({
+let { domain, extensions, fields } = await publicClient.getEip712Domain({
   address: '0x57ba3ec8df619d4d243ce439551cce713bb17411'
 })
+
+console.log(domain, extensions, fields);
+
+// read EIP-712 domain on contract that has not been deployed by providing deployment factory
+({ domain, extensions, fields } = await publicClient.getEip712Domain({
+  address: '0x57ba3ec8df619d4d243ce439551cce713bb17411',
+  factory: factory.address,
+  factoryData: encodeFunctionData({
+    abi: factory.abi,
+    functionName: 'createAccount',
+    args: ['0x0000000000000000000000000000000000000000', 0n]
+  }),
+}))
 
 console.log(domain, extensions, fields)
