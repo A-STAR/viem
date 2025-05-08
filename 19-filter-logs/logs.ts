57e608e1accedc0df08941c2b4073d1aa4d2790d
A-STAR <andreysuperstar@gmail.com>
2025-05-09 00:02:54 +0300

    feat(logs): get event logs

---
+++ 19-filter-logs/logs.ts
@@ -0,0 +1,37 @@
+import { parseAbiItem } from 'viem'
+import { publicClient } from './client'
+import { wagmiAbi } from './abi'
+
+const filter = await publicClient.createContractEventFilter({
+  address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  abi: wagmiAbi,
+  eventName: 'Transfer'
+})
+
+let logs = await publicClient.getFilterChanges({ filter })
+
+console.log(logs)
+
+let eventFilter = await publicClient.createEventFilter({
+  address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  event: parseAbiItem('event Transfer(address indexed, address indexed, uint256)'),
+})
+
+let eventLogs = await publicClient.getFilterChanges({
+  filter: eventFilter
+})
+
+console.log(eventLogs)
+
+eventFilter = await publicClient.createEventFilter({
+  address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  event: parseAbiItem('event Transfer(address indexed, address indexed, uint256)'),
+})
+
+eventLogs = await publicClient.getFilterLogs({ filter: eventFilter })
+
+console.log(eventLogs)
+
+logs = await publicClient.getLogs()
+
+console.log(logs)
